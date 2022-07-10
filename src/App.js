33b2f74756ca7d454f39d52ba7fa52b737bef3b4
Rudresh Pandey <rudreshpandey2002@gmail.com>
2022-07-10 15:46:37 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 
+import React,{ useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+
+
+
 function App() {
+  const[mode,setMode] = useState('light')
+  const[alert,setAlert] = useState(null);
+  
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+  }
+  const toggleMode = ()=>{
+    if(mode==='light'){
+    setMode('dark')
+    document.body.style.backgroundColor='black'
+    showAlert("DarkMode enable", "success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showAlert("LightMode enable", "success")
+    } 
+  }
+
   return (
     <>
-
-
-      <Navbar title="TextUtils" aboutme="About" />
-      <div className="container my-4" >
-      <TextForm titlename=""/>
-      {/* <About/> */}
-      </div>
-
+      <Router>
+      <Navbar title="TextUtils" aboutme="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      <div className="container my-4" > 
+      <Switch>
+          <Route path="/about" >
+            <About />
+          </Route>
+          <Route path="/" >
+            <TextForm showAlert={showAlert} titlename="Enter text" mode={mode}  />
+          </Route>
+      </Switch>
+      </div>  
+      </Router>
     </>
 
   );
+
 
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,6 +4,7 @@ export default function TextForm(props) {
     {
         let newText = text.toUpperCase();
         useText(newText) 
+        props.showAlert("changed to uppercase","success")
     }
     const HandelOnChange = (event)=>
     {
@@ -27,6 +28,7 @@ export default function TextForm(props) {
     {
       let newText = ""
       useText(newText)
+      props.showAlert("Text cleard","warning")
     }
     // let newStyle={
     //   color : 'red'
@@ -36,9 +38,11 @@ export default function TextForm(props) {
     const[newStyle,setNewStyle] = useState({
         color : 'black'
     })
+    // const len = text.length;
+    // len===0?(text.split(" ").length)-1:text.split(" ").length    My method to count the number of words
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
         <h3>{props.titlename}</h3>
         <div className="mb-3">
         <textarea className="form-control" style={newStyle} value={text} onChange={HandelOnChange} id="exampleFormControlTextarea1" rows="6"></textarea>
@@ -47,9 +51,9 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={ChangeColor}>Change Color</button>
         <button className="btn btn-primary mx-2" onClick={Clear}>Clear</button>
     </div>
-    <div className="container my-4">
+    <div className="container my-4" style={{color: props.mode==='dark'?'white':'black'}}>
       <h2>Text Summary: </h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{text.length>0 ? text.trim().split(" ").length : 0} words and {text.length} characters</p>
     </div>
 
     </>
