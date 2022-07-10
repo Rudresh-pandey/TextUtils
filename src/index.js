b7d4dfef0da232431759e1da806693ef0699d625
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-07-10 22:56:18 +0530

    fix: 🐞 routing fixed
    tranferred the router from app.js to index.js and removed the strict mode
    
    - Abhishek Kushwaha

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import About from './components/About';
+import TextForm from './components/TextForm';
+import reportWebVitals from './reportWebVitals';
+import { Router , Route , Switch } from 'react-router-dom';
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/"  component={App} />
+        <Route path="/home" component={TextForm} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </Router>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
